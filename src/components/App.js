0b70637fb0514e4f6b1824b9001ad6be7ce057ce
Jordan Kittle <shwackd@gmail.com>
2021-02-10 22:55:25 -0800

    Add Nav Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../css/index.css';
 import SearchForm from './SearchForm';
+import Nav from './Nav';
 
 function App() {
 	return (
 		<div className="container">
 			<SearchForm />
+      <Nav />
 		</div>
 	);
 }
