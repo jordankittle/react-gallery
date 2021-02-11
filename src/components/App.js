da1f496c74bdd16e7dbd53eb2ad89c7cefbdd996
Jordan Kittle <shwackd@gmail.com>
2021-02-11 02:22:18 -0800

    display fetched images

--- src/components/App.js
+++ src/components/App.js
@@ -13,47 +13,7 @@ import apiKey from '../config.js';
 
 class App extends Component {
  
-  constructor() {
-    super();
-    this.state = {
-      images: [],
-      loading: true
-    };
-  } 
 
-  componentDidMount(){
-    const defaultSearch = 'cats';
-    this.handleSearch(defaultSearch);  
-  }
-
-  handleSearch = (query = 'cats') => {
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${query}&privacy_filter=1&per_page=24&format=json&nojsoncallback=1`;
-    fetch(url)
-    .then ( response => response.json())
-    .then( response => {
-      // handle success
-      const imagesReturned = response.photos.photo.map( photo => {
-        return {
-          url: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`,
-          id: photo.id,
-          alt: photo.title
-        };
-      });
-
-      this.setState({
-        images: imagesReturned,
-        loading: false
-      });
-    })
-    .catch( error => {
-      // handle error
-      console.log('Error fetching and parsing data', error);
-    })
-    .then( () => {
-      // always executed
-      console.log('Fetch attempt complete');
-    });  
-  }
   
 
   render(){
@@ -63,11 +23,11 @@ class App extends Component {
           <SearchForm />
           <Nav />
           <Switch>
-            <Route exact path="/" render={ () => <PhotoContainer query ="cats" />} />
-            <Route exact path="/cats" render={ () => <PhotoContainer query ="cats" />} />
-            <Route exact path="/dogs" render={ () => <PhotoContainer query="dogs" />} />
-            <Route exact path="/computers" render={ () => <PhotoContainer query="computers" />} />
-            <Route path="/search/:query" render={ ({match}) => <PhotoContainer query={match.params.query} />} />
+            <Route exact path="/" render={ ({match}) => <PhotoContainer query ="cats" apiKey={apiKey} match={match} />} />
+            <Route exact path="/cats" render={ ({match}) => <PhotoContainer query ="cats" apiKey={apiKey} match={match} />} />
+            <Route exact path="/dogs" render={ ({match}) => <PhotoContainer query="dogs" apiKey={apiKey} match={match} />} />
+            <Route exact path="/birds" render={ ({match}) => <PhotoContainer query="birds" apiKey={apiKey} match={match} />} />
+            <Route path="/search/:query" render={ ({match}) => <PhotoContainer query={match.params.query} apiKey={apiKey} match={match} />} />
             <Route component={ NotFound } />
           </Switch>
         </div>
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -4,16 +4,65 @@ import Photo from './Photo';
 class PhotoContainer extends Component {
 
 	state = {
+		images: [],
+		loading: true
+	}
+	
+	
+	componentDidMount(){
+		this.handleSearch(this.props.query);  
+		console.log('PhotoContainer mounted');
+	}
 
-	};
+	componentDidUpdate(prevProps) {
 
+        if (this.props.match.path !== prevProps.match.path) {
+            this.handleSearch(this.props.query);
+        }
+    }
 
+	
+	handleSearch = (query = 'cats') => {
+		const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.props.apiKey}&text=${query}&privacy_filter=1&per_page=24&format=json&nojsoncallback=1`;
+		fetch(url)
+		.then ( response => response.json())
+		.then( response => {
+			// handle success
+			const imagesReturned = response.photos.photo.map( photo => {
+				return {
+				url: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`,
+				id: photo.id,
+				alt: photo.title
+				};
+		  	});
+	
+			this.setState({
+				images: imagesReturned,
+				loading: false
+			});
+
+		})
+		.catch( error => {
+			// handle error
+			console.log('Error fetching and parsing data', error);
+		})
+		.then( () => {
+			// always executed
+			console.log('Fetch attempt complete');
+		});  
+	} 
+	
 	render() {
+
 		return(
             <div className="photo-container">
             <h2>Results</h2>
             <ul>
-				<Photo url="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="test" />
+				{
+					this.state.images.map((image, index) => {
+						return <Photo url={image.url} alt={image.alt} key={image.id} />
+					})
+				}
             </ul>
           </div>
 		);
